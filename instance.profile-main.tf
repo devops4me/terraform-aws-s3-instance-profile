f264aa73232b9f78a5ddb61b1d7214e118f3b300
apollo <apolloakora@gmail.com>
2019-01-10 19:49:57 +0000

    roles issue fix

--- instance.profile-main.tf
+++ instance.profile-main.tf
@@ -17,7 +17,7 @@ resource aws_iam_policy s3_access_policy
 resource aws_iam_policy_attachment collection
 {
     name       = "s3-attach-policy-${ var.in_ecosystem_name }-${ var.in_tag_timestamp }"
-    role       = "${ aws_iam_role.iam_s3_access_role.name }"
+    roles      = [ "${ aws_iam_role.iam_s3_access_role.name }" ]
     policy_arn = "${ aws_iam_policy.s3_access_policy.arn }"
 }
 
@@ -25,5 +25,5 @@ resource aws_iam_policy_attachment collection
 resource aws_iam_instance_profile s3_access_profile
 {
     name  = "s3-profile-${ var.in_ecosystem_name }-${ var.in_tag_timestamp }"
-    roles = [ "${ aws_iam_role.iam_s3_access_role.name }" ]
+    role  = "${ aws_iam_role.iam_s3_access_role.name }"
 }
