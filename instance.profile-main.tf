9959c774071136fb41644e9e88b52fe51ba85278
apollo <apolloakora@gmail.com>
2019-01-10 18:34:07 +0000

    Used variable stamps to tag role policy and profile names.

--- instance.profile-main.tf
+++ instance.profile-main.tf
@@ -1,15 +1,14 @@
 
-
 resource aws_iam_role iam_s3_access_role
 {
-    name               = "s3-role-${ local.ecosystem_id }-${ module.resource-tags.out_tag_timestamp }"
+    name               = "s3-role-${ var.in_ecosystem_name }-${ var.in_tag_timestamp }"
     assume_role_policy = "${ file( "${path.module}/instance.profile-role.json" ) }"
 }
 
 
 resource aws_iam_policy s3_access_policy
 {
-    name        = "s3-policy-${ local.ecosystem_id }-${ module.resource-tags.out_tag_timestamp }"
+    name        = "s3-policy-${ var.in_ecosystem_name }-${ var.in_tag_timestamp }"
     description = "Base policy for access to s3 buckets."
     policy      = "${ file( "${path.module}/instance.profile-s3policy.json" ) }"
 }
@@ -17,7 +16,7 @@ resource aws_iam_policy s3_access_policy
 
 resource aws_iam_policy_attachment collection
 {
-    name       = "s3-attach-policy-${ local.ecosystem_id }-${ module.resource-tags.out_tag_timestamp }"
+    name       = "s3-attach-policy-${ var.in_ecosystem_name }-${ var.in_tag_timestamp }"
     roles      = [ "${ aws_iam_role.iam_s3_access_role.name }" ]
     policy_arn = "${ aws_iam_policy.s3_access_policy.arn }"
 }
@@ -25,6 +24,6 @@ resource aws_iam_policy_attachment collection
 
 resource aws_iam_instance_profile s3_access_profile
 {
-    name  = "s3-profile-${ local.ecosystem_id }-${ module.resource-tags.out_tag_timestamp }"
+    name  = "s3-profile-${ var.in_ecosystem_name }-${ var.in_tag_timestamp }"
     roles = [ "${ aws_iam_role.iam_s3_access_role.name }" ]
 }
